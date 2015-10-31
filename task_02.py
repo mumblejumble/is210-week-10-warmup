ac2ef2ae9810cc26a1ce95f9b75e6660eca7d4ed
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-31 02:35:41 +0000

    extracting keys

---
+++ task_02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Working with data module """
+
+
+import data
+
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+
+BAND_NAMES = data.BANDS.keys()
